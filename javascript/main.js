cf8747edfa8523dff5628af074a4341b144ef51f
Robert Haye <roberthaye1994@gmail.com>
2022-04-16 02:09:58 +0200

    commit 24

--- javascript/main.js
+++ javascript/main.js
@@ -30,7 +30,7 @@ if (freelanceStatus == "Available") {
 
 function mainText() {
     var typed = new Typed('#typing1', {
-        strings: ["", "web designer", "web developer", "software developer", "avid gamer"],
+        strings: ["", "web designer", "web developer", "avid gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         startDelay: 0,
@@ -106,31 +106,31 @@ navLinks.forEach((link) => {
     });
 });
 /* ===================== Mailto JS ===================== */
- var now = new Date();
- var time = d.getHours()
+var now = new Date();
+var time = d.getHours()
 
- function sendMail() {
+function sendMail() {
 
-     const greetingTypes = ["Good morning", "Good afternoon", "Good evening"];
-     var name = document.getElementById("cname").value;
-     var email = document.getElementById("cemail").value;
-     var subject = document.getElementById("csubject").value;
-     var body = document.getElementById("cmessage").value;
+    const greetingTypes = ["Good morning", "Good afternoon", "Good evening"];
+    var name = document.getElementById("cname").value;
+    var email = document.getElementById("cemail").value;
+    var subject = document.getElementById("csubject").value;
+    var body = document.getElementById("cmessage").value;
 
 
-     if (time < 12) {
-         greetingText = greetingTypes[0];
-     } else if (time < 18) {
-         greetingText = greetingTypes[1];
-     } else {
-         greetingText = greetingTypes[2];
-     }
+    if (time < 12) {
+        greetingText = greetingTypes[0];
+    } else if (time < 18) {
+        greetingText = greetingTypes[1];
+    } else {
+        greetingText = greetingTypes[2];
+    }
 
-     var ebody = greetingText + " Robert Haye," + "%0D%0A" + body + "%0D%0A" + "%0D%0A" + "My information:" + "%0D%0A" + "Name: " + name + "%0D%0A" + "Email: " + email + "%0D%0A" + "%0D%0A";
+    var ebody = greetingText + " Robert Haye," + "%0D%0A" + body + "%0D%0A" + "%0D%0A" + "My information:" + "%0D%0A" + "Name: " + name + "%0D%0A" + "Email: " + email + "%0D%0A" + "%0D%0A";
 
-     if (subject === "") {
-         window.location.replace(`mailto:haye.robert@yahoo.com?body=${ebody}&subject=${name} wants to contact you`);
-     } else {
-         window.location.replace(`mailto:haye.robert@yahoo.com?body=${ebody}&subject=${subject}`);
-     }
- }
+    if (subject === "") {
+        window.location.replace(`mailto:haye.robert@yahoo.com?body=${ebody}&subject=${name} wants to contact you`);
+    } else {
+        window.location.replace(`mailto:haye.robert@yahoo.com?body=${ebody}&subject=${subject}`);
+    }
+}
